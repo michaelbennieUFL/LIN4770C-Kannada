284b6e066779f83df94a1939999a8082900089a4
webbuf <zwebb@ufl.edu>
2024-04-16 17:35:41 -0400

    more results. all of them?

--- analysis.py
+++ analysis.py
@@ -13,16 +13,17 @@ df.dropna(inplace=True)
 
 random_classifier = DummyClassifier(strategy = 'uniform', random_state = 727)  
 
-df_features = df[['a', 'e', 'i', 'o', 'u']]
+#df_features = df[['a', 'e', 'i', 'o', 'u']]
+df_features = df[[ 'm', 'n', 'ɳ']]
 #df_features = df.drop(['a', 'e', 'i', 'o', 'u', 'class'], axis=1)
 #df_features = df.drop('class', axis=1)
 print(df_features)
 DT_classifier = DecisionTreeClassifier(random_state=727)
-#scores_SVM = cross_validate(DT_classifier, df_features, df['class'], cv=10, scoring=["precision", "recall", "f1"])
-scores_Rand = cross_validate(random_classifier, df_features, df['class'], cv=10, scoring=["precision", "recall", "f1"])
+scores_SVM = cross_validate(DT_classifier, df_features, df['class'], cv=10, scoring=["precision", "recall", "f1"])
+#scores_Rand = cross_validate(random_classifier, df_features, df['class'], cv=10, scoring=["precision", "recall", "f1"])
 
-precision = sum(scores_Rand['test_precision'])/len(scores_Rand['test_precision'])
-recall = sum(scores_Rand['test_recall'])/len(scores_Rand['test_recall'])
-f1 = sum(scores_Rand['test_f1'])/len(scores_Rand['test_f1'])
+precision = sum(scores_SVM['test_precision'])/len(scores_SVM['test_precision'])
+recall = sum(scores_SVM['test_recall'])/len(scores_SVM['test_recall'])
+f1 = sum(scores_SVM['test_f1'])/len(scores_SVM['test_f1'])
 
 print(precision, recall, f1)
